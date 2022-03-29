83bc60c3a58c780eed29dd8f25d6200f292914bc
kuldeep kumar <kuldeepgoswami1129@gmail.com>
2022-03-29 09:11:59 +0530

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>E-commerce Project</title>
-        <!-- Latest compiled and minified CSS -->
+    <title>E-comm Project</title>
+    <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
@@ -14,26 +14,45 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-
     {{View::make('header')}}
     @yield('content')
     {{View::make('footer')}}
 
-    
 </body>
 <style>
     .custom-login{
         height: 500px;
         padding-top: 100px;
     }
-    .img.slider-img{
-        height : 400px !important;
+    img.slider-img{
+        height: 400px !important
     }
     .custom-product{
-        height: 600px;
+        height: 600px
     }
     .slider-text{
-        background-color:aqua !important;
+        background-color: #35443585 !important;
+    }
+    .trending-image{
+        height: 100px;
+    }
+    .trending-item{
+        float: left;
+        width: 20%;
+    }
+    .trending-wrapper{
+        margin: 30px;
+    }
+    .detail-img{
+        height: 200px;
+    }
+    .search-box{
+        width: 500px !important
+    }
+    .cart-list-devider{
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 20px;
+        padding-bottom: 20px
     }
 </style>
 </html>
